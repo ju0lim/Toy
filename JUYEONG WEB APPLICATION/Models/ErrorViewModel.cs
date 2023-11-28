6f8adba5e3874db68ede0fe181403e64106ee855
ju0lim <juyeonglim@naver.com>
2023-11-28 15:39:53 +0900

    2023/11/28

--- JUYEONG WEB APPLICATION/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace JUYEONG_WEB_APPLICATION.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
